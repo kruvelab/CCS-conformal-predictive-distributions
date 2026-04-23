03e31b08b8f9929dc6d1a03f4c5c476b507d2821
lucferrplo <ferrando1299@gmail.com>
2026-04-23 16:20:31 +0200

    Update structures.py

--- OODDS_CPS_GraphCCS/structures.py
+++ OODDS_CPS_GraphCCS/structures.py
@@ -204,7 +204,7 @@ def construct_graph(molecular_identifiers):
         The atom types are represented as atom attributes of the nodes.
     """
     try:
-        graph =  mol2graph(Chem.MolFromSmiles(molecular_identifiers['Absolute SMILES']))
+        graph = mol2graph(Chem.MolFromSmiles(molecular_identifiers['Absolute SMILES']))
     except Exception:
         try:
             graph = mol2graph(Chem.MolFromInchi(molecular_identifiers.InChI))
